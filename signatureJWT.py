40e3c13aff4dcd9cde5c0b605c0dc8a275699cc9
root <beliezerpotocki@gmail.com>
2025-12-19 16:49:56 -0500

    first commit

---
+++ signatureJWT.py
@@ -0,0 +1,16 @@
+from cryptography.hazmat.primitives import hmac
+from cryptography.hazmat.primitives import hashes
+import json
+import base64
+
+def generateSignature(secret, headerAndPayload):
+    h = hmac.HMAC( secret.encode(), hashes.SHA256())
+    h.update( headerAndPayload.encode() )
+    return base64.urlsafe_b64encode( h.finalize() )
+
+def signJWT(secret, jwt):
+    splitedJWT = jwt.split('.')
+    splitedJWT[2] = generateSignature(secret, f'{splitedJWT[0]}.{splitedJWT[1]}').decode()
+    return '.'.join(splitedJWT)
+
+print( signJWT('secret1', 'a.b.c') )
